04c49bd5538faefd7fc84c979d415dafe1bd6cee
Cody Cogbill <codeman24@gmail.com>
2023-04-13 16:17:27 -0600

    done

--- index.js
+++ index.js
@@ -1,42 +1,67 @@
-// Write your solution in this file!
-let employee = {
+// // Write your solution in this file!
+// let employee = {
 
-    name: "Cody"
+//     name: "Cody"
 
-}
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+//     let employees = { ...employee }
+
+//     employees[key] = value
+
+//     return employees
+
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+//     employee[key] = value
+
+//     return employee
+
+// }
+
+// function deleteFromEmployeeByKey(employee, key) {
+
+//     let newEmployee = { ...employee }
+
+//     delete newEmployee[key]
+
+//     return newEmployee
+
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+//     delete employee[key]
+
+//     return employee
+
+// }
+
+const employee = {
+  name: "Cody",
+  streetAddress: "123 Street",
+};
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-
-    let employees = { ...employee }
-
-    employees[key] = value
-
-    return employees
-
+  return Object.assign({}, employee, { [key]: value });
 }
-
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
 
-    employee[key] = value
-
-    return employee
-
+  return employee;
 }
 
 function deleteFromEmployeeByKey(employee, key) {
-
-    let newEmployee = { ...employee }
-
-    delete newEmployee[key]
-
-    return newEmployee
-
+  let newEmp = Object.assign({}, employee);
+  delete newEmp[key];
+  return newEmp;
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
-
-    delete employee[key]
-
-    return employee
-
+  delete employee[key];
+  return employee;
 }
